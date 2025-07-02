27b6e8aeb120629c29af5005bfa05796fccb1183
Dipak Singh <dipaksingh3323@gmail.com>
2025-07-02 16:53:57 +0530

    Hero section page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import { useLocation } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Home from './pages/Home';
 function App() {
   const isOnwerPath = useLocation().pathname.includes("owner");
 
   return (
     <>
       {!isOnwerPath && <Navbar />}
+      <div className='min-h-[70vh]'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </div>
     </>
   )
 }
